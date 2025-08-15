7a6ecacbecde94b32364126b9905251bfdcfd435
Arun <arunbana612@gmail.com>
2025-08-15 12:49:25 +0530

    update1

--- basic/p1.py
+++ basic/p1.py
@@ -1,7 +1,7 @@
-#print("hello everyone\ntoday i am starting python")
-#print("hello"+" "+"all")
-#  print("hello")    <----X  due to space it is compile time error 
-#print("Hello " + input("what is your name ->"))
-# print( len( input("what is your name ? ")))
-# print(len(input("what is your name ? ")))
+print("hello everyone\ntoday i am starting python")
+print("hello"+" "+"all")
+ print("hello")    <----X  due to space it is compile time error 
+print("Hello " + input("what is your name ->"))
+print( len( input("what is your name ? ")))
+print(len(input("what is your name ? ")))
 
